88c0dcc32f580259a4beb2b3368d595ac62fd7c8
Shahriar-Samir <shahriar.samir80@gmail.com>
2025-04-28 10:40:18 +0600

    updated practice1

--- practice/practice1.cpp
+++ practice/practice1.cpp
@@ -4,6 +4,12 @@ int main(){
     // this is a comment
     int age = 20;
     double gpa = 5;
-    std:: cout << "My name is Samir. My age is " << age << ". And my cgpa is " << gpa << std:: endl;
+    bool isGraduated = true;
+    if(isGraduated){
+        std:: cout << "My name is Samir. My age is " << age << ". And my cgpa is " << gpa <<  ". I'm graduated is true"<< std:: endl ;
+    }
+    else{
+        std:: cout << "Not true" << std::endl;
+    }
     return 0;
 }
