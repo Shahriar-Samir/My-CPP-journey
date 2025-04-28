37108e1c548dd5d896e502e562160e475c648779
Shahriar-Samir <shahriar.samir80@gmail.com>
2025-04-28 11:25:44 +0600

    added: practice3

---
+++ practice/practice3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main(){
+    double x = (int) 20.455;
+    char y = 20;
+    std:: cout << x << std::endl;
+    std:: cout << (int) y << std::endl;
+    std:: cout << (double) 5 << std::endl;
+    return 0;
+}
