e578f701c4230a26811537736f3e8ce179c72fc3
Shahriar-Samir <shahriar.samir80@gmail.com>
2025-04-28 11:49:40 +0600

    add: practice4

---
+++ practice/practice4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main(){
+    std::string name;
+    std:: cout << "What is your name ? ";
+    std:: cin >> name;
+    std:: cout << "Your name is " << name << "." << std::endl;
+    return 0;
+}
