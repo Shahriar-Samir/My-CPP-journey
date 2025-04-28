0de29279740b736ac8d072b698241b6bcdebc2d4
Shahriar-Samir <shahriar.samir80@gmail.com>
2025-04-28 12:18:46 +0600

    update

--- practice/practice5.cpp
+++ practice/practice5.cpp
@@ -13,10 +13,16 @@ int main(){
     double z1 = sqrt(20);
     double z2 = ceil(20.5);
     double z3 = floor(20.5);
+    double z4 = pow(2,4);
+    double z5 = round(20.8);
+    double z6 = abs(-20.5);
     std:: cout << c << std::endl;
     std:: cout << d << std::endl;
     std:: cout << z1 << std::endl;
     std:: cout << z2 << std::endl;
     std:: cout << z3 << std::endl;
+    std:: cout << z4 << std::endl;
+    std:: cout << z5 << std::endl;
+    std:: cout << z6 << std::endl;
     return 0;
 }
