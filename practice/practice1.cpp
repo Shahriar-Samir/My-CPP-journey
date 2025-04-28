f47f1bdf9aca08e0776eab7eb1216fdf54c3e89d
Shahriar-Samir <shahriar.samir80@gmail.com>
2025-04-28 10:33:53 +0600

    initalized c++ practice file

---
+++ practice/practice1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    // this is a comment
+    int age = 20;
+    double gpa = 5;
+    std:: cout << "My name is Samir. My age is " << age << ". And my cgpa is " << gpa << std:: endl;
+    return 0;
+}
